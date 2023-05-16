090a06ee893a84d4d378f470e42ea79337fc5a0b
NicoDelgadoCentennial <ndelgad4@my.centennialcollege.ca>
2023-05-15 22:12:42 -0400

    Helper, model and middlewares done

---
+++ Class2/models/post.model.js
@@ -0,0 +1,73 @@
+const filename = '../data/posts.json'
+let posts = require(filename)
+const helper = require('../helper.js')
+
+function getPosts() {
+    return new Promise((resolve,reject) => {
+        if (posts.length === 0) {
+            reject({
+                message: 'no posts available',
+                status: 202
+            })
+        }
+        resolve(posts)
+    })
+}
+
+function getPost(id) {
+    return new Promise((resolve,reject) => {
+        helper.mustBeInArray(posts, id)
+        .then(post => resolve(post))
+        .catch(err => reject(err))
+    })
+}
+function insertPost(newPost) {
+    return new Promise((resolve,reject) =>{
+        const id= {id: helper.getNewId(posts)}
+        const date ={
+            createdAt: helper.newDate(),
+            updatedAt: helper.newDate()
+        }
+        newPost = {...id,...date,...newPost}
+        posts.push(newPost)
+        helper.writeJSONFile(filename,posts)
+        resolve(newPost, 200)
+    })
+}
+
+function updatePost(id, newPost) {
+    return new Promise((resolve,reject) =>{
+        helper.mustBeInArray(posts, id)
+        .then(post => {
+            const index = posts.findIndex(p => p.id === id)
+            const date ={
+                createdAt: posts[index].createdAt,
+                updatedAt: helper.newDate()
+            }
+            posts[index] = {...id,...date,...newPost}
+            helper.writeJSONFile(filename,posts)
+            resolve(posts[index])
+            })
+            .catch(error => reject(error))
+    })
+}
+
+function deletePost(id) {
+    return new Promise((resolve,reject) =>{
+        helper.mustBeInArray(posts, id)
+        .then(() => {
+            posts.posts.filter(p => p.id !==id)
+            helper.writeJSONFile(filename,posts)
+            resolve(posts)
+            })
+            .catch(error => reject(error))
+            })
+}
+
+module.exports= {
+    getPosts,
+    getPost,
+    insertPost,
+    updatePost,
+    deletePost
+}
