ce642012f396735b05d82090993f215671b653c2
NicoDelgadoCentennial <ndelgad4@my.centennialcollege.ca>
2023-06-12 15:14:57 -0400

    Website content finisher

---
+++ Class3/crudApp/src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/app/home/home.component.html'));
+});
+
+const port = 3000; 
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
